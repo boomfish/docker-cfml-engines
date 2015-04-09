d932d00f637b995033ccb0e5f34c5fc466e2f589
Dennis Clark <boomfish@gmail.com>
2015-04-09 15:02:47 +1000

    Use LD_LIBRARY_PATH instead of -Djava.library.path

--- railo/setenv.sh
+++ railo/setenv.sh
@@ -7,10 +7,14 @@
 # Default memory settings if not specified in $RAILO_JAVA_OPTS
 : ${RAILO_JAVA_OPTS:="-Xms256m -Xmx512m -XX:MaxPermSize=128m"}
 
-# Activate Java agent and Apache Tomcat native library support
-JAVA_OPTS="${RAILO_JAVA_OPTS} -javaagent:lib/railo-inst.jar -Djava.library.path=/usr/lib/x86_64-linux-gnu";
+# Activate Java agent 
+JAVA_OPTS="${RAILO_JAVA_OPTS} -javaagent:lib/railo-inst.jar";
 
 # additional JVM arguments can be added to the above line as needed, such as
 # custom Garbage Collection arguments.
 
 export JAVA_OPTS;
+
+# Add location of Apache Tomcat native library to the library path
+LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu";
+export LD_LIBRARY_PATH;
